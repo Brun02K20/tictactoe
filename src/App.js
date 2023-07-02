3dabe2b79b42c02daabb885997daaf84fbaf3326
Brun02K23 <bvirinni@gmail.com>
2023-07-02 20:11:53 -0300

    estadisticas basicas hechas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import { Board } from './components/Board.js';
 
-function App() {
+const App = () => {
+  const [xWins, setXWins] = useState(0);
+  const [oWins, setOWins] = useState(0);
+  const [draws, setDraws] = useState(0);
+
+  const handleWin = (winner) => {
+    if (winner === 'X') {
+      setXWins(xWins + 1);
+    } else if (winner === 'O') {
+      setOWins(oWins + 1);
+    } else {
+      setDraws(draws + 1);
+    }
+  };
+
   return (
-    <div className="App">
+    <div className="app">
       <h1>Tic Tac Toe</h1>
-      <Board />
+      <div className="statistics">
+        <p>Wins for X: {xWins}</p>
+        <p>Wins for O: {oWins}</p>
+        <p>Draws: {draws}</p>
+      </div>
+      <Board handleWin={handleWin} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,72 @@
 import React, { useState } from 'react';
-import { Square } from './Square.js';
+import { Square } from './Square';
 
-// Función para calcular el ganador del juego
+const Board = ({ handleWin }) => {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
+    const [gameResult, setGameResult] = useState(null);
+
+    const handleClick = (index) => {
+        const newSquares = [...squares];
+
+        if (calculateWinner(newSquares) || newSquares[index]) {
+            return;
+        }
+
+        newSquares[index] = xIsNext ? 'X' : 'O';
+        setSquares(newSquares);
+        setXIsNext(!xIsNext);
+
+        const winner = calculateWinner(newSquares);
+        if (winner) {
+            handleWin(winner);
+            setGameResult(`Winner: ${winner}`);
+        } else if (newSquares.every((square) => square !== null)) {
+            handleWin('Draw');
+            setGameResult('Draw');
+        }
+    };
+
+    const renderSquare = (index) => {
+        return (
+            <Square
+                value={squares[index]}
+                onClick={() => handleClick(index)}
+            />
+        );
+    };
+
+    const handleRestart = () => {
+        setSquares(Array(9).fill(null));
+        setXIsNext(true);
+        setGameResult(null);
+    };
+
+    return (
+        <div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+            <h3>{gameResult}</h3>
+            <button className="restart-button" onClick={handleRestart}>
+                Restart Game
+            </button>
+        </div>
+    );
+};
+
 const calculateWinner = (squares) => {
     const lines = [
         [0, 1, 2],
@@ -22,72 +87,6 @@ const calculateWinner = (squares) => {
     }
 
     return null;
-}
-
-const Board = () => {
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [xIsNext, setXIsNext] = useState(true);
-
-    const handleClick = (index) => {
-        const newSquares = [...squares];
-
-        if (calculateWinner(newSquares) || newSquares[index]) {
-            return;
-        }
-
-        newSquares[index] = xIsNext ? 'X' : 'O';
-        setSquares(newSquares);
-        setXIsNext(!xIsNext);
-    };
-
-    const handleRestart = () => {
-        setSquares(Array(9).fill(null));
-        setXIsNext(true);
-    };
-
-    const renderSquare = (index) => {
-        return (
-            <Square
-                value={squares[index]}
-                onClick={() => handleClick(index)}
-            />
-        );
-    };
-
-    const winner = calculateWinner(squares);
-    let status;
-    if (winner) {
-        status = `Winner: ${winner}`;
-    } else if (squares.every((square) => square !== null)) {
-        status = 'It\'s a draw!';
-    } else {
-        status = `Next player: ${xIsNext ? 'X' : 'O'}`;
-    }
-
-    return (
-        <div>
-            <div className="status">{status}</div>
-            <div className="board-row">
-                {renderSquare(0)}
-                {renderSquare(1)}
-                {renderSquare(2)}
-            </div>
-            <div className="board-row">
-                {renderSquare(3)}
-                {renderSquare(4)}
-                {renderSquare(5)}
-            </div>
-            <div className="board-row">
-                {renderSquare(6)}
-                {renderSquare(7)}
-                {renderSquare(8)}
-            </div>
-            <button className="restart-button" onClick={handleRestart}>
-                Restart Game
-            </button>
-        </div>
-    );
 };
 
 export { Board };
-
